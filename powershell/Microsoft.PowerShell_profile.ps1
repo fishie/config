9a1a295c94ba5a168c6b4b1418bd9123cd9ad842
Rishie Sharma <rishie@rishie.se>
2021-03-02 21:17:34 +0100

    Added PowerShell config

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+﻿### In admin PowerShell:
+# PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+# New-Item -ItemType SymbolicLink -Path "$env:userprofile\Documents\WindowsPowerShell" -Target "$env:userprofile\Documents\PowerShell"
+#
+# Then move this file to: %userprofile%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# posh-git
+Import-Module posh-git
+$GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "[HH:mm:ss]") '
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::DarkGray
