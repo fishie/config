f7b26ad8888c467480a439d7aa5dd872c0c28fe7
Rishie Sharma <rishie@rishie.se>
2022-02-19 09:49:39 +0100

    Added search function

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,12 @@ end
 function fish_user_key_bindings
   bind \cd beginning-of-line kill-line
 end
+
+function search
+  echo -e "\033[1;33myay\033[0m" &&
+  unbuffer yay --singlelineresults --topdown -Ss $argv | head -n -2 | head -n 5 &&
+  echo -e "\n\033[1;33msnap\033[0m" &&
+  unbuffer snap find $argv | head -n 5 &&
+  echo -e "\n\033[1;33mflatpak\033[0m" &&
+  unbuffer flatpak search --columns=name,version,description $argv | head -n 5
+end
