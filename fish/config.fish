a2e7b628beba563e07e7b46a1453784ca5c1a266
Rishie Sharma <rishie@rishie.se>
2026-03-31 09:27:58 +0200

    Fish: on empty prompt, tab only lists folders

--- fish/config.fish
+++ fish/config.fish
@@ -23,3 +23,15 @@ function smart_escape
     end
 end
 bind \e smart_escape
+
+# On empty prompt, tab only lists folders
+function tab_or_cd_complete
+    if test -z (commandline)
+        commandline -i 'cd '
+        commandline -f complete
+    else
+        commandline -f complete
+    end
+end
+
+bind \t tab_or_cd_complete
